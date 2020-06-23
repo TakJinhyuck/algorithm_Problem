edb4d8c4644510a0fa1cf50cecbb574610f24cba
JinHyuck(KOSTA) <knd5191@naver.com>
2020-06-24 01:41:03 +0900

    .

---
+++ src/p2577/Main.java
@@ -0,0 +1,14 @@
+package p2577;
+
+public class Main {
+
+	public static void main(String[] args) {
+		int A = 150;
+		int B = 266;
+		int C = 427;
+		int [] arr = new int [10];//0~9±îÁö
+		int total = A*B*C;
+		System.out.println(total%10);
+	}
+
+}
